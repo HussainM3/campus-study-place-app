041c778ee9eec15a00e663d25e645760f0094422
Hussain Muhammed <hussainmohd2003@outlook.com>
2025-12-03 18:02:11 -0500

    Removed default code, added initial setup for app

---
+++ src/data/places.js
@@ -0,0 +1,25 @@
+const places = [
+  {
+    id: 1,
+    name: "Thode Library - Basement",
+    noise: "Quiet",
+    seating: "Individual",
+    nearFood: false,
+  },
+  {
+    id: 2,
+    name: "LR Wilson Building",
+    noise: "Moderate",
+    seating: "Group",
+    nearFood: false,
+  },
+  {
+    id: 3,
+    name: "Mills Library - 2nd Floor",
+    noise: "Loud",
+    seating: "Group",
+    nearFood: true,
+  }
+];
+
+export default places;
